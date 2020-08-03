d4c355d497d8b42764cdd4b4faa1258d4e8be2a3
Johan <johan@johanvanhelden.com>
2020-08-03 19:56:22 +0200

    more cleanup and fixes

--- .circleci/scripts/validation.sh
+++ .circleci/scripts/validation.sh
@@ -22,58 +22,22 @@ if [ $? != 0 ]; then
     exit 1;
 fi
 
-if [ -f "$HOME/configfiles/.php_cs" ]; then
-    echo
-    echo '============================='
-    echo '| Coding standard fixers:    '
-    echo '============================='
-    php-cs-fixer fix app config tests --dry-run --diff --allow-risky=yes --config=$HOME/configfiles/.php_cs
-    if [ $? != 0 ]; then
-        exit 1;
-    fi
+echo
+echo '============================='
+echo '| Coding standard fixers:    '
+echo '============================='
+php-cs-fixer fix app config tests --dry-run --diff --allow-risky=yes --config=.php_cs
+if [ $? != 0 ]; then
+    exit 1;
 fi
 
 echo
 echo '============================='
-echo '| Missing docblocks:         '
+echo '| Mess detector:             '
 echo '============================='
-hasMissing=false
-
-# iterate over all methods in our codebase and their previous line as well (this should be the docblock closing tag)
-while read -r line; do
-    # skip grep separators
-    if [ "$line" == '--' ]; then
-        continue
-    fi
-
-    # skip docblock closing tags
-    if [[ "$line" =~ '*/' ]]; then
-        continue
-    fi
-
-    # skip the method line itself
-    if [[ "$line" =~ 'function' ]]; then
-        continue
-    fi
-
-    # any left-over lines are not closing docblock tags and thus methods without docblocks
-    hasMissing=true
-    echo "Missing docblock detected: $line"
-done <<<$(grep -r -B 1 --include=*.php ".*function.*)$" app tests)
-
-if $hasMissing; then
+phpmd app text phpmd.xml
+if [ $? != 0 ]; then
     exit 1;
-fi
-
-if [ -f "phpmd.xml" ]; then
-    echo
-    echo '============================='
-    echo '| Mess detector:             '
-    echo '============================='
-    phpmd app text phpmd.xml
-    if [ $? != 0 ]; then
-        exit 1;
-    fi
 fi
 
 echo
